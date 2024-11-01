4f8532b21c6b45c85553602bf2eb48a2d24dea9d
ToastedToast <hey@toasted.dev>
2024-11-01 16:18:27 +0800

    feat(commands): add !daily command

---
+++ src/commands/daily.ts
@@ -0,0 +1,23 @@
+import type { Command } from "@/utils/commands";
+
+import { Users } from "@/utils/db";
+
+export default {
+  name: "daily",
+  aliases: ["last24h", "pointslast24h", "dailypoints"],
+  run: ({ interaction }) => {
+    let user = Users.get(interaction.author.id)!;
+    if (user.mainId) {
+      user = Users.get(user.mainId)!;
+    }
+
+    const allUsers = Users.getAll();
+    const rank = allUsers
+      .sort((a, b) => b.dailyPoints - a.dailyPoints)
+      .findIndex(u => u.id === user.id) + 1;
+
+    interaction.reply(
+      `${interaction.author.name}, you have gained ${user.dailyPoints} points in the last 24 hours, and are rank #${rank}.`,
+    );
+  },
+} satisfies Command;
