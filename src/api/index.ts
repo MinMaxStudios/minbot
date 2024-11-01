cb4e0d26e0d2a17d9107eb7f8ae21d303071a915
ToastedToast <hey@toasted.dev>
2024-11-01 23:03:15 +0800

    fix(linking): don't count alts in totals and lb

--- src/api/index.ts
+++ src/api/index.ts
@@ -10,6 +10,7 @@ api.get("/lb/points", (c) => {
   return c.json({
     users: users
       .sort((a, b) => b.points - a.points)
+      .filter(user => !user.mainId)
       .map(user => ({
         id: user.id,
         name: user.name,
@@ -24,6 +25,7 @@ api.get("/lb/daily-points", (c) => {
   return c.json({
     users: users
       .sort((a, b) => b.dailyPoints - a.dailyPoints)
+      .filter(user => !user.mainId)
       .map(user => ({
         id: user.id,
         name: user.name,
@@ -36,7 +38,7 @@ api.get("/lb/daily-points", (c) => {
 api.get("/total", (c) => {
   const users = Users.getAll();
   return c.json({
-    points: users.reduce((acc, user) => acc + user.points, 0),
+    points: users.filter(user => !user.mainId).reduce((acc, user) => acc + user.points, 0),
     messages: getMessages(),
   });
 });
@@ -45,4 +47,9 @@ api.get("/count", (c) => {
   return c.json({
     count: getCount(),
   });
+});
+
+Bun.serve({
+  fetch: api.fetch,
+  port: Number.parseInt(process.env.PORT ?? "3000"),
 });
