a5499b64026a937146ec5f2f800b045027ad359b
ToastedToast <hey@toasted.dev>
2024-11-01 21:08:49 +0800

    fix(linking): allow unlink command to be run from main user

--- src/commands/unlink.ts
+++ src/commands/unlink.ts
@@ -6,18 +6,37 @@ export default {
   name: "unlink",
   run: ({ interaction }) => {
     const user = Users.get(interaction.author.id)!;
-    if (!user.mainId)
-      throw interaction.reply(`${interaction.author.name}, you are not linked to any account.`);
+    const allUsers = Users.getAll();
+    const linkedUsers = allUsers.filter(u => u.mainId === interaction.author.id);
+    if (user.mainId) {
+      const mainUser = Users.get(user.mainId)!;
+      Users.update(user.mainId, {
+        points: mainUser.points - user.points,
+        dailyPoints: mainUser.dailyPoints - user.dailyPoints,
+      });
+      Users.update(interaction.author.id, {
+        mainId: undefined,
+      });
 
-    const mainUser = Users.get(user.mainId)!;
-    Users.update(user.mainId, {
-      points: mainUser.points - user.points,
-      dailyPoints: mainUser.dailyPoints - user.dailyPoints,
-    });
-    Users.update(interaction.author.id, {
-      mainId: undefined,
-    });
+      interaction.reply(`${interaction.author.name}, successfully unlinked your account.`);
+    }
+    else if (linkedUsers.length) {
+      const mainUser = Users.get(interaction.author.id)!;
+      let points = 0;
+      let dailyPoints = 0;
+      for (const user of linkedUsers) {
+        points += user.points;
+        dailyPoints += user.dailyPoints;
+        Users.update(user.id, {
+          mainId: undefined,
+        });
+      }
+      Users.update(interaction.author.id, {
+        points: mainUser.points - points,
+        dailyPoints: mainUser.dailyPoints - dailyPoints,
+      });
 
-    interaction.reply(`${interaction.author.name}, successfully unlinked your account.`);
+      interaction.reply(`${interaction.author.name}, successfully unlinked your accounts.`);
+    }
   },
 } satisfies Command;
